d32e50e5584ff0ddcdaf81c3434ac346fb462578
Son <json9512@gmail.com>
2020-09-03 16:35:24 +1000

    change cors

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,8 @@ app.use(favicon(path.join(__dirname, 'favicon.ico')))
 // Allow cors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, GET, POST, PUT, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
